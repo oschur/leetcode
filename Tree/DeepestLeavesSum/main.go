1a7d153907e76b706514cbe3106ca7f9976704d3
oschur <genda5656@gmail.com>
2026-01-10 00:36:27 +0500

    add solution to problem №1302

---
+++ Tree/DeepestLeavesSum/main.go
@@ -0,0 +1,33 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deepestLeavesSum(root *TreeNode) int {
+	sum := 0
+	expDepth := findTreeDepth(root)
+	sum += addDeepestLeaveToSum(root, expDepth, 0)
+	return sum
+}
+
+func findTreeDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(findTreeDepth(root.Left), findTreeDepth(root.Right))
+}
+
+func addDeepestLeaveToSum(root *TreeNode, expDepth, curDepth int) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil && curDepth == expDepth {
+		return root.Val
+	}
+
+	return addDeepestLeaveToSum(root.Left, expDepth, curDepth+1) + addDeepestLeaveToSum(root.Right, expDepth, curDepth+1)
+}
