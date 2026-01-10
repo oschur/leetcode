1e5968777585419fc83596486b160ce0b5a28e8a
oschur <genda5656@gmail.com>
2026-01-10 23:46:47 +0500

    add BFS solution to problem №104

--- Tree/MaximumDepthOfBinaryTree/main.go
+++ Tree/MaximumDepthOfBinaryTree/main.go
@@ -6,14 +6,43 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
+func maxDepthByDfs(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	left := maxDepth(root.Left)
-	right := maxDepth(root.Right)
+	left := maxDepthByDfs(root.Left)
+	right := maxDepthByDfs(root.Right)
 	if left > right {
 		return 1 + left
 	}
 	return 1 + right
+}
+
+func maxDepthByBfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	depth := 0
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		depth++
+
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return depth
 }
