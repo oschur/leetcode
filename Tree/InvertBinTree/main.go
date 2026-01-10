3fa1255cf17b366bfd6ed6269cc595d4caeb894d
oschur <genda5656@gmail.com>
2026-01-11 01:08:03 +0500

    add BFS solution to problen №226

--- Tree/InvertBinTree/main.go
+++ Tree/InvertBinTree/main.go
@@ -6,7 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func invertTree(root *TreeNode) *TreeNode {
+func invertTreeDFS(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
@@ -15,7 +15,31 @@ func invertTree(root *TreeNode) *TreeNode {
 		Val: root.Val,
 	}
 
-	res.Left = invertTree(root.Right)
-	res.Right = invertTree(root.Left)
+	res.Left = invertTreeDFS(root.Right)
+	res.Right = invertTreeDFS(root.Left)
 	return &res
+}
+
+func invertTreeBFS(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		node.Left, node.Right = node.Right, node.Left
+
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+
+	return root
 }
