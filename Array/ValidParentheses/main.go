a2b2f216cbf30b855526781131ade182a8dada59
oschur <genda5656@gmail.com>
2026-01-12 02:52:24 +0500

    add solition to problem №20

---
+++ Array/ValidParentheses/main.go
@@ -0,0 +1,25 @@
+package main
+
+func isValid(s string) bool {
+	stack := []rune{}
+
+	pairs := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, c := range s {
+		switch c {
+		case '(', '{', '[':
+			stack = append(stack, c)
+		case ')', '}', ']':
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[c] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
