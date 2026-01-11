311f53488062dda41c41be86d897219f9002e7ff
oschur <genda5656@gmail.com>
2026-01-12 00:52:53 +0500

    add solution to problem №125

---
+++ Array/ValidPalindrome/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "strings"
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	l, r := 0, len(s)-1
+
+	for l < r {
+		if (s[l] < 'a' || s[l] > 'z') && (s[l] < '0' || s[l] > '9') {
+			l++
+			continue
+		}
+
+		if (s[r] < 'a' || s[r] > 'z') && (s[r] < '0' || s[r] > '9') {
+			r--
+			continue
+		}
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+
+	return true
+}
