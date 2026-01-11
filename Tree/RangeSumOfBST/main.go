96aa065dda74bf50f39a53abe9f97db2a2118746
oschur <genda5656@gmail.com>
2026-01-12 03:15:03 +0500

    add solution to problem №938

---
+++ Tree/RangeSumOfBST/main.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	sum := 0
+	if root == nil {
+		return 0
+	}
+
+	if root.Val >= low && root.Val <= high {
+		sum += root.Val
+	}
+
+	if root.Val > low {
+		sum += rangeSumBST(root.Left, low, high)
+	}
+
+	if root.Val < high {
+		sum += rangeSumBST(root.Right, low, high)
+	}
+	return sum
+}
