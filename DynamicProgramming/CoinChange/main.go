67bf6326bc317ccb4df063b54dab39aa40a0531f
oschur <genda5656@gmail.com>
2026-01-17 03:13:20 +0500

    add solution to problem №322

---
+++ DynamicProgramming/CoinChange/main.go
@@ -0,0 +1,23 @@
+package main
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		dp[i] = 100000 // по-хорошему надо импортировать math.MaxInt32 но из констрейтов ясно что это число подойдет
+	}
+
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if coin <= i {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+
+	if dp[amount] > amount {
+		return -1
+	}
+
+	return dp[amount]
+}
