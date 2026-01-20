2c7ff08528eecfd6585fba37dcbc01cfabaa07ec
oschur <genda5656@gmail.com>
2026-01-20 20:35:27 +0500

    add alternative soluton to problem №268

--- Array/MissingNumber/main.go
+++ Array/MissingNumber/main.go
@@ -16,3 +16,13 @@ func missingNumber(nums []int) int {
 	}
 	return sumWithMissing - sumWithoutMissing
 }
+
+// Binary solution
+
+func missingNumberBinary(nums []int) int {
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		res = res ^ (i + 1) ^ nums[i]
+	}
+	return res
+}
