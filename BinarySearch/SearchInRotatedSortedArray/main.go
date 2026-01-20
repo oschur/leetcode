975f0b7142a19e32305a8992d7ada4386ae604d2
oschur <genda5656@gmail.com>
2026-01-20 19:04:22 +0500

    add solution to problem №33

---
+++ BinarySearch/SearchInRotatedSortedArray/main.go
@@ -0,0 +1,26 @@
+package main
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		}
+		if nums[m] < nums[r] {
+			if target > nums[m] && target <= nums[r] {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
+		} else {
+			if target < nums[m] && target >= nums[l] {
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		}
+	}
+	return -1
+}
