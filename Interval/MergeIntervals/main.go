9c933e2ecfa8755444448e321e29b7b22c766d25
oschur <genda5656@gmail.com>
2026-01-21 18:09:11 +0500

    add solution to problem №56

---
+++ Interval/MergeIntervals/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	res := [][]int{}
+	cur := intervals[0]
+
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] <= cur[1] {
+			if intervals[i][1] > cur[1] {
+				cur[1] = intervals[i][1]
+			}
+		} else {
+			res = append(res, cur)
+			cur = intervals[i]
+		}
+	}
+	res = append(res, cur)
+	return res
+}
