ffc18ab068806203009e82b9052119c709824f8b
oschur <genda5656@gmail.com>
2026-01-29 20:26:59 +0500

    add solution to problem №21

---
+++ LinkedList/MergeTwoLists/main.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	if list2.Val < list1.Val {
+		list2.Next = mergeTwoLists(list2.Next, list1)
+		return list2
+	} else {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+}
