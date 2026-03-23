5736920de4c964ee497744de67112680c859e34c
oschur <genda5656@gmail.com>
2026-03-24 02:10:26 +0500

    add another solution to problem №33

--- BinarySearch/SearchInRotatedSortedArray/main.go
+++ BinarySearch/SearchInRotatedSortedArray/main.go
@@ -24,3 +24,37 @@ func search(nums []int, target int) int {
 	}
 	return -1
 }
+
+func findOffset(nums []int) int {
+	l, r := -1, len(nums)-1
+	for r-l > 1 {
+		m := l + (r-l)/2
+		if nums[m] > nums[len(nums)-1] {
+			l = m
+		} else {
+			r = m
+		}
+	}
+	return r
+}
+
+func searchAnother(nums []int, target int) int {
+	offset := findOffset(nums)
+	l, r := 0, len(nums)
+
+	for r-l > 1 {
+		m := l + (r-l)/2
+		mid := (m + offset) % len(nums)
+		if nums[mid] <= target {
+			l = m
+		} else {
+			r = m
+		}
+	}
+
+	realLeft := (l + offset) % len(nums)
+	if nums[realLeft] == target {
+		return realLeft
+	}
+	return -1
+}
